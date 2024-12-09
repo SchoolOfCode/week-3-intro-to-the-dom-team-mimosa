0694f9bc2a619f367b5a7d097246235c7dce10ec
Monika <mciskowska@gmail.com>
2024-12-09 13:51:38 +0000

    worked throught ticket 1d and 1e

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ console.log(document.getElementById("title"))
 console.log(document.getElementsByClassName("content"))
 
 console.log(document.getElementsByTagName(`<li>`))
+
+console.log(document.querySelector(".highlight")); //1d
+
+console.log(document.querySelectorAll(".highlight"));
