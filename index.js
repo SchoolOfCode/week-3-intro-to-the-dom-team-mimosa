8d13cedf45efd79eca15d3a6417f1aa8a992470f
BeardedGrinn <mitchell.t.grinnell@hotmail.co.uk>
2024-12-09 13:51:26 +0000

    My changes

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ console.log(document.getElementById("title"))
 console.log(document.getElementsByClassName("content"))
 
 console.log(document.getElementsByTagName(`<li>`))
+
+console.log(document.querySelector(".highlight"))
