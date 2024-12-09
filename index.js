61db36157055b861ba1ae1f5e2abc83e73c605f0
BeardedGrinn <mitchell.t.grinnell@hotmail.co.uk>
2024-12-09 13:10:09 +0000

    Added and linked index.js file
    Completed tasks 1a-1c

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("linked!")
+
+console.log(document.getElementById("title"))
+
+console.log(document.getElementsByClassName("content"))
+
+console.log(document.getElementsByTagName(`<li>`))
